c4d3ec4783b8c5d7b18dfe65ce311de7ca7689d8
Inho Kang <ikang9712@gmail.com>
2023-07-16 15:48:16 +0900

    presentation layer display news widget

---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,55 @@
+import 'package:flut_demo/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:flut_demo/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+import 'package:flut_demo/features/daily_news/presentation/widgets/article_tile.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppbar(),
+      body: _buildBody(),
+    );
+  }
+
+  _buildAppbar() {
+    return AppBar(
+      title: const Text(
+        "Daily News",
+        style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticlesBloc, RemoteArticleState>(
+        builder: (_, state) {
+      if (state is RemoteArticlesLoading) {
+        return const Center(
+          child: CupertinoActivityIndicator(),
+        );
+      }
+      if (state is RemoteArticlesError) {
+        return const Center(
+          child: Icon(Icons.refresh),
+        );
+      }
+      if (state is RemoteArticlesDone) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            print(state.articles![index].urlToImage);
+            return ArticleWidget(
+              article: state.articles![index],
+            );
+          },
+          itemCount: state.articles!.length,
+        );
+      }
+      return const SizedBox();
+    });
+  }
+}
